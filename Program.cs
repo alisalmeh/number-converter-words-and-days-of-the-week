cc873369758c21443d9b15f286d81b466d395816
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-01 11:45:07 +0330

    add PrintMenu method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace AliSalmeh_ProjectWeek4
     {
         static void Main(string[] args)
         {
-            
+            Console.Clear();
+            PrintMenu();
+
+
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("WELCOME");
+            Console.WriteLine("Enter a number to execute:\n" +
+                                "1) Convert number to alphabet\n" +
+                                "2) Convert number to day of week\n" +
+                                "3) Exit");
+            Console.Write("--> ");
         }
     }
 }
