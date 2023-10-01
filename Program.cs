578653f1778aa8aba25932a427c96f044fc4811e
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-01 12:21:47 +0330

    add ConvertNumberToAlphabet method

--- Program.cs
+++ Program.cs
@@ -9,7 +9,24 @@ namespace AliSalmeh_ProjectWeek4
             Console.Clear();
             PrintMenu();
 
+            var input = Convert.ToInt32(Console.ReadLine());
 
+            if (input == 1)
+            {
+                ConvertNumberToAlphabet();
+            }
+
+        }
+
+        public static void ConvertNumberToAlphabet()
+        {
+            Console.WriteLine("Enter a number between 1 to 10: ");
+            var numToAlphInput = Convert.ToInt32(Console.ReadLine());
+
+            var numberToWord = new string[] { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
+
+            var result = (numToAlphInput >= 1 && numToAlphInput <= 10) ? $"{numberToWord[numToAlphInput - 1]}" : "You entered out of range!";
+            Console.WriteLine($"--> Result: {result}");
         }
 
         public static void PrintMenu()
